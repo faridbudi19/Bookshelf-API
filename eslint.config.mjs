52373d4f64c2da0a8c4db259d69ba8de835e92dd
Farid Budi <faridbudi1987@gmail.com>
2024-10-14 20:59:37 +0700

    x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import pluginJs from "@eslint/js";
 import daStyle from 'eslint-config-dicodingacademy';
 
 export default [
+  daStyle,
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
-  daStyle,
 ];
